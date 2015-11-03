8a89f22bfcd3f0f27000e34068c88222c5886a42
Fannon <heimlersimon@gmail.com>
2015-11-03 16:35:44 +0100

    Reimplemented FakeId

--- IDProvider.functions.php
+++ IDProvider.functions.php
@@ -29,20 +29,19 @@ class IDProviderFunctions {
 
 		$type = $params['type'];
 		$prefix = $params['prefix'] ?: '';
+		$padding = $params['prefix'] ?: 0;
 		$wikipage = $params['wikipage'] ?: false;
 
 		$id = null;
 
-		// UUID Provider
 		if ($type === 'uuid') {
-
 			$id = $prefix . self::getUUID();
 
-
-			// Increment Provider
 		} else if ($type === 'increment') {
+			$id = self::getIncrement($prefix, $padding);
 
-			$id = self::getIncrement($params['prefix'], $params['padding']);
+		} else if ($type === 'fakeid') {
+			$id = $prefix .  self::getFakeId();
 
 		} else { // No valid option
 			throw new Exception('Unknown type');
@@ -63,7 +62,7 @@ class IDProviderFunctions {
 		if ($id) {
 			return $id;
 		} else {
-			throw new Exception('No ID was calculated!');
+			throw new Exception('No valid ID was calculated!');
 		}
 
 
@@ -87,7 +86,6 @@ class IDProviderFunctions {
 		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 
     }
-
 
 	public static function getIncrement($prefix, $padding = 0) {
 
@@ -194,173 +192,170 @@ class IDProviderFunctions {
 
 	}
 
+	/**
+	 * Generates a Fake ID that is very likely to be truly unique (no guarantee however!)
+	 *
+	 * This is achived through mixing a milli-timestamp (php uniqid();) with a random string
+	 *
+	 * @return string
+	 */
+	public static function getFakeId() {
 
-    /**
-    * Substitutes ___TIMESTAMP___ for the current UNIX timestamp
-    *
-    * @param  [string] $oldText original mediawiki text
-    * @return [string]          substituted mediawiki text
-    */
-    function substituteTimestamp($oldText) {
+		// Generates a random string of length 1-2.
+		$id = base_convert(rand(0, 36^2), 10, 36);
 
-    $date = new DateTime();
-    $timestamp = $date->getTimestamp();
-    $newText = str_replace("___TIMESTAMP___", $timestamp, $oldText);
+		// This will "compress" the uniqid (some sort of microtimestamp) to a more dense string
+		$id .= base_convert(uniqid(), 10, 36);
 
-    return $newText;
-    }
+		return $id;
+	}
 
-    /**
-    * Substitutes ___RANDOMNUMBER___ with a random number
-    * Minimum and maximum are defined through $wgSubstitutorMinRand and $wgSubstitutorMaxRand
-    *
-    * @param  [string] $oldText original mediawiki text
-    * @return [string]          substituted mediawiki text
-    */
-    function substituteRandomNumber($oldText) {
+	/**
+	 * returns a random number between $wgSubstitutorMinRand and $wgSubstitutorMaxRand
+	 *
+	 * @param $min
+	 * @param $max
+	 *
+	 * @return int
+	 */
+	public static function getRandomNumber($min, $max) {
 
-    $pattern = "/___RANDOMNUMBER___/";
-    $newText = preg_replace_callback($pattern, "generateRandomNumberCallback", $oldText);
+		global $wgIDProviderMinRandNumber;
+		global $wgIDProviderMaxRandNumber;
 
-    return $newText;
-    }
-
-    /**
-    * Substitutes ___RANDOMSTRING___ with a random string of length $wgSubstitutorRandStringLength
-    *
-    * @param  [string] $oldText original mediawiki text
-    * @return [string]          substituted mediawiki text
-    */
-    function substituteRandomString($oldText) {
-
-    $pattern = "/___RANDOMSTRING___/";
-    $newText = preg_replace_callback($pattern, "generateRandomStringCallback", $oldText);
-
-    return $newText;
-    }
+		self::toJSON($wgIDProviderMinRandNumber);
 
 
-    /**
-    * Substitutes ___FAKEID___ with a fake ID, that should be unique
-    * (No garantuees however!)
-    *
-    * @param  [string] $oldText original mediawiki text
-    * @return [string]          substituted mediawiki text
-    */
-    function substituteFakeID($oldText) {
 
-    $pattern = "/___FAKEID___/";
-    $newText = preg_replace_callback($pattern, "generateFakeIDCallback", $oldText);
+		if (!$min) {
+			$min = $wgIDProviderMinRandNumber;
+		}
 
-    return $newText;
-    }
+		if (!$max) {
+			$max = $wgIDProviderMaxRandNumber;;
+		}
+
+		return rand($min, $max);
+	}
+
+	/**
+	 * Callback function that returns a random string with length of $wgSubstitutorRandStringLength
+	 *
+	 * @param integer $length
+	 *
+	 * @return string
+	 */
+	public static function getRandomString($length) {
+
+		global $wgIDProviderRandStringLength;
+
+		if (!$length) {
+			$length = $wgIDProviderRandStringLength;
+		}
+
+		$key = '';
+		$keys = array_merge(range(0,9), range('a', 'z'));
+
+		for($i=0; $i < $length; $i++) {
+			$key .= $keys[array_rand($keys)];
+		}
+
+		return $key;
+	}
 
 
-    /**
-    * Substitutes ___SHORTID___ with a shorter fake ID, that should be unique
-    *
-    * @param  [string] $oldText original mediawiki text
-    * @return [string]          substituted mediawiki text
-    */
-    function substituteShortID($oldText) {
 
-    $pattern = "/___SHORTID___/";
-    $newText = preg_replace_callback($pattern, "generateShortIDCallback", $oldText);
+	// Old Substitutor Functions
+	// TODO: Reimplement them
 
-    return $newText;
-    }
+
+//    /**
+//    * Substitutes ___TIMESTAMP___ for the current UNIX timestamp
+//    *
+//    * @param  string $oldText
+//    * @return [string]          substituted mediawiki text
+//    */
+//
+//	/**
+//	 * @param string $oldText 	original mediawiki text
+//	 * @return string
+//	 */
+//    public static function substituteTimestamp($oldText) {
+//
+//		$date = new DateTime();
+//		$timestamp = $date->getTimestamp();
+//		$newText = str_replace("___TIMESTAMP___", $timestamp, $oldText);
+//
+//		return $newText;
+//    }
+//
+//    /**
+//    * Substitutes ___RANDOMNUMBER___ with a random number
+//    * Minimum and maximum are defined through $wgSubstitutorMinRand and $wgSubstitutorMaxRand
+//    *
+//    * @param  [string] $oldText original mediawiki text
+//    * @return [string]          substituted mediawiki text
+//    */
+//    function substituteRandomNumber($oldText) {
+//
+//    $pattern = "/___RANDOMNUMBER___/";
+//    $newText = preg_replace_callback($pattern, "generateRandomNumberCallback", $oldText);
+//
+//    return $newText;
+//    }
+//
+//    /**
+//    * Substitutes ___RANDOMSTRING___ with a random string of length $wgSubstitutorRandStringLength
+//    *
+//    * @param  [string] $oldText original mediawiki text
+//    * @return [string]          substituted mediawiki text
+//    */
+//    function substituteRandomString($oldText) {
+//
+//		$pattern = "/___RANDOMSTRING___/";
+//		$newText = preg_replace_callback($pattern, "generateRandomStringCallback", $oldText);
+//
+//		return $newText;
+//    }
+//
+//
+//    /**
+//    * Substitutes ___FAKEID___ with a fake ID, that should be unique
+//    * (No garantuees however!)
+//    *
+//    * @param  [string] $oldText original mediawiki text
+//    * @return [string]          substituted mediawiki text
+//    */
+//    function substituteFakeID($oldText) {
+//
+//		$pattern = "/___FAKEID___/";
+//		$newText = preg_replace_callback($pattern, "generateFakeIDCallback", $oldText);
+//
+//		return $newText;
+//    }
+//
+//
+//    /**
+//    * Substitutes ___SHORTID___ with a shorter fake ID, that should be unique
+//    *
+//    * @param  [string] $oldText original mediawiki text
+//    * @return [string]          substituted mediawiki text
+//    */
+//    function substituteShortID($oldText) {
+//
+//		$pattern = "/___SHORTID___/";
+//		$newText = preg_replace_callback($pattern, "generateShortIDCallback", $oldText);
+//
+//		return $newText;
+//    }
+
+
 
 
 
     //////////////////////////////////////////
     // HELPER / CALLBACK FUNCTIONS          //
     //////////////////////////////////////////
-
-    /**
-    * Callback function that returns a random number between $wgSubstitutorMinRand and $wgSubstitutorMaxRand
-    *
-    * @return [integer]
-    */
-    public function generateRandomNumberCallback() {
-
-        global $wgSubstitutorMinRand;
-        global $wgSubstitutorMaxRand;
-
-        return rand($wgSubstitutorMinRand, $wgSubstitutorMaxRand);
-    }
-
-    /**
-    * Callback function that returns a random string with length of $wgSubstitutorRandStringLength
-    *
-    * @return [string]
-    */
-    public static function generateRandomStringCallback() {
-
-        global $wgSubstitutorRandStringLength;
-
-        return generateRandomString($wgSubstitutorRandStringLength);
-    }
-
-
-
-    /**
-    * Generates a Fake ID that is very likely to be truly unique (no guarantee however!)
-    *
-    * This is achived through mixing a militimestamp (php uniqid();) with a random string
-    *
-    * @return [string]
-    */
-    public static function generateFakeIDCallback() {
-
-        $id = self::generateRandomString(4);
-        $id .= self::uniqid();
-
-        return $id;
-    }
-
-    /**
-    * Generates a Fake ID that is very likely to be truly unique (no guarantee however!)
-    * This version tries to be as short as possible
-    *
-    * This is achived through mixing a militimestamp (php uniqid();) with a random string
-    *
-    * @return [string]
-    */
-    public static function generateShortIDCallback() {
-
-        // Generates a random string of length 1-2.
-        $id = base_convert(rand(0, 36^2), 10, 36);
-
-        // This will "compress" the uniqid (some sort of microtimestamp) to a more dense string
-        $id .= base_convert(uniqid(), 10, 36);
-
-        return $id;
-    }
-
-    /**
-    * Rturns a random string with length of $length
-    *
-    * @param  [number] $length string length
-    * @return [string]
-    */
-    public static function generateRandomString($length) {
-
-        global $wgSubstitutorRandStringLength;
-
-        if (!$length) {
-            $length = $wgSubstitutorRandStringLength;
-        }
-
-        $key = '';
-        $keys = array_merge(range(0,9), range('a', 'z'));
-
-        for($i=0; $i < $length; $i++) {
-            $key .= $keys[array_rand($keys)];
-        }
-        return $key;
-    }
-
-
 
 	/**
 	 * Debug function that converts an object/array to a <pre> wrapped pretty printed JSON string
